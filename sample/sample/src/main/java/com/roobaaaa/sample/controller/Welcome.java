56a6380472dc7f1ce653937b0cf7dbed5ab8c43e
ROOBASRI T M <108512576+roobasrirooba@users.noreply.github.com>
2023-08-18 20:52:01 +0530

    Add files via upload

---
+++ sample/sample/src/main/java/com/roobaaaa/sample/controller/Welcome.java
@@ -0,0 +1,12 @@
+package com.roobaaaa.sample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Welcome {
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome";
+	}
+}
