d2a4396902ba09297437a0d8b10150df180b2d30
ROOBASRI T M <108512576+roobasrirooba@users.noreply.github.com>
2023-08-22 15:51:46 +0530

    Delete 3 day/DAY_3_CLASS_EXERCISE directory

--- 3 day/DAY_3_CLASS_EXERCISE/src/main/java/com/example/demo/Repository/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.EmployeeModel;
-
-public interface EmployeeRepo extends JpaRepository<EmployeeModel, Integer>{
-
-}
